74a6adcbf41c9c1ab5e67ee5a78d6ad052381fc4
richard-brosler-senai <richard.brosler@sp.senai.br>
2024-03-18 22:03:39 -0300

    Conteúdo da aula do dia 18-03-2024

--- NP24_Java2_B/src/br/com/senaisp/bauru/richard/secao03/ShoppingCart01.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/richard/secao03/ShoppingCart01.java
@@ -4,12 +4,13 @@ public class ShoppingCart01 {
     public static void main(String[] args) {
         // Declare and initialize String variables.  Do not initialize message yet.
     	String custName = "Richard";
-    	String itemDesc = "Camisa Social";
+    	String itemDesc = "Camisa\" Social";
 
     	// Assign the message variable 
         String message = custName + " comprou " + 1 + " " + itemDesc;
         
         // Print and run the code
-        System.out.println(message);
+        System.out.print(message);
+        System.out.println(" olá");
     }
 }
