e41e2ecc38fb08acf4f4756e55dc596ff94362ae
richard-brosler-senai <richard.brosler@sp.senai.br>
2024-03-28 20:24:44 -0300

    Conteúdo da aula do dia 28-03-2024

--- NP24_Java2_B/src/br/com/senaisp/bauru/richard/licao07/exercicio/TesteJogo.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/richard/licao07/exercicio/TesteJogo.java
@@ -7,6 +7,10 @@ public class TesteJogo {
 		System.out.println(ct01);
 		Cartao ct02 = new Cartao();
 		System.out.println(ct02);
+		//Instanciando terminal
+		Terminal tm = new Terminal();
+		
+		
 		//Criando um máquina
 		MaquinaFliperama m1 = new MaquinaFliperama("MK1");
 		m1.lerCartao(ct01);
