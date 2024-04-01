8b507bd4c2b94150ae301f0f911d77761857722f
richard-brosler-senai <richard.brosler@sp.senai.br>
2024-04-01 19:30:56 -0300

    Conteúdo da aula do dia 01-04-2024

--- NP24_Java2_B/src/br/com/senaisp/bauru/richard/licao08/exercicio/Carta.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/richard/licao08/exercicio/Carta.java
@@ -7,4 +7,47 @@ public class Carta {
 	private int naipe;
 	private int valor;
 	private String numero;
+	//Constructor
+	public Carta(int nap, int num) {
+		//Tratando o Naipe para que seja de 0 a 3
+		if (nap<0 || nap>3) {
+			throw new RuntimeException("Os naipes devem ser "
+					+ "de 0 a 3");
+		}
+		//Tratando o número para que seja de 0 a 12
+		if (num<0 || num>12) {
+			throw new RuntimeException("Os números devem"
+					+ " ser de 0 a 12");
+		}
+		//Atribuindo os fields 
+		naipe = nap;
+		valor = num;
+		numero = numeros[num]; 
+	}
+	
+	public String getNaipe() {
+		return naipes[naipe];
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public int getValor() {
+		return valor > 9 ? 10 : valor + 1;
+	}
+	
+	@Override
+	public String toString() {
+					  //1234567
+		String apoio = "╔═════╗\n" //1
+					+  "║##   ║\n" //2
+					+  "║  §  ║\n" //3
+					+  "║   ##║\n" //4
+					+  "╚═════╝\n";//5
+		apoio = apoio.replaceFirst("##", getNumero());
+		apoio = apoio.replace("§", getNaipe());
+		apoio = apoio.replaceFirst("##", getNumero());
+		return apoio;
+	}
 }
